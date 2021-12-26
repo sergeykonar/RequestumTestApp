fcb5ad59e531f576914c603926c7818a19454940
serge <ksA2701200@>
2021-12-26 14:44:50 +0100

    Text watcher adapter

--- app/src/main/java/com/example/requestumtestapp/MainActivity.kt
+++ app/src/main/java/com/example/requestumtestapp/MainActivity.kt
@@ -1,22 +1,19 @@
 package com.example.requestumtestapp
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.requestumtestapp.adapter.BreweryAdapter
+import com.example.requestumtestapp.adapter.TextWatcherAdapter
 import com.example.requestumtestapp.interfaces.MapCallback
 import com.example.requestumtestapp.model.Brewery
 import com.example.requestumtestapp.viewmodel.MainViewModel
-import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -42,20 +39,13 @@ class MainActivity : AppCompatActivity() {
         breweryListView = findViewById(R.id.breweriesList)
         searchView = findViewById(R.id.searchEditText)
 
-        searchView.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
+        searchView.addTextChangedListener(object :
+            TextWatcherAdapter() {
             override fun afterTextChanged(p0: Editable?) {
+                super.afterTextChanged(p0)
                 val query = p0.toString()
                 mainViewModel.search(query)
             }
-
         })
     }
 
